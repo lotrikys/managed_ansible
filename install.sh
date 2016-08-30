3ded6be99a9924ab230e04b98fd168b03c29107e
root <root@v-2-eu22-d661-vm-50.webazilla.com>
2016-08-30 20:43:38 +0200

    1

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /bin/cp -rf ./apache/* /etc/httpd/conf/
-/bin/cp -rf ./geoip/* /etc/httpd/conf.d/
+/bin/cp -rf ./remoteip/* /etc/httpd/conf.d/
 /bin/cp -rf ./nginx/* /etc/nginx/
 /bin/cp -rf ./mysql/* /etc/
